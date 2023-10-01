0c147ecf9bc3685f2c7e1883325eae15bc8a0f44
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2023-10-01 12:32:41 +0100

    updated figure 3 in response to reviewer comment

--- scripts/3_reliability.R
+++ scripts/3_reliability.R
@@ -199,11 +199,126 @@ reliability_alluvial <- all_responses %>%
 #         axis.text.x = element_text(colour="black"))+
 #   ggtitle("Were you aware of the use of transmission models in informing public health policy?")
 
-plot_grid(ggplot(data = reliability_alluvial%>% filter(prior_reliability_1_10!="Did not answer",
+# plot_grid(ggplot(data = reliability_alluvial%>% filter(prior_reliability_1_10!="Did not answer",
+#                                                        during_reliability_1_10!="Did not answer"),
+#                  aes(axis1 = prior_reliability_1_10, axis2 = during_reliability_1_10,
+#                      y = freq, freq2 = freq2))+
+#             scale_x_discrete(limits = c("Prior to the COVID-19 pandemic", "During the COVID-19 pandemic"), 
+#                              expand = c(.2, .05),
+#                              labels = function(x) str_wrap(x, width = 12)) +
+#             labs(x="Time period",y="Number of respondents",fill="Reliability score during the pandemic")+
+#             theme_classic()+
+#             facet_wrap(~platform_label_generic,scales="free_y")+
+#             geom_alluvium(aes(fill=during_reliability_1_10))+
+#             geom_stratum()+
+#             scale_fill_viridis_d(begin=1,end=0)+
+#             geom_text(stat = "stratum", aes(label = after_stat(paste0(stratum))),
+#                       size=2.5,min.y=4) +
+#             theme(legend.position = "bottom",
+#                   axis.line = element_blank(),strip.text = element_text(size=12),
+#                   axis.text.x = element_text(colour="black"))+
+#             ggtitle("On a scale of 1-10 with 1 being “extremely unreliable” and 10 being “extremely \nreliable” how do you feel about the use of transmission models in informing \npublic health policy?"),
+#           ggplot(reliability_time_summary,aes(x=difference,y=perc,#x=platform_label,
+#                                               group=platform_label_generic,col=platform_label_generic,
+#                                               fill=platform_label_generic))+
+#             geom_col(position="dodge")+
+#             theme_bw()+
+#             scale_colour_manual(values=c("#1e2761","#408ec6"))+
+#             scale_fill_manual(values=c("#1e2761","#408ec6"))+
+#             scale_x_continuous(breaks = seq(-10,10,1))+
+#             theme(legend.position="bottom",strip.background.x = element_rect(fill="white"))+
+#             labs(x="Difference in reliability scores (during - before)",y="Percentage of \nrespondents (%)",
+#                  col="",fill="")+
+#             geom_vline(xintercept = 0.5,linetype="dashed")+
+#             geom_vline(xintercept = -0.5,linetype="dashed")+
+#             annotate("text",x=5,y=40,label="Reliability score increased \nduring the pandemic")+
+#             annotate("segment", x = 0.5, xend = 10, y = 40, yend = 40, colour = "black", size=1, alpha=0.6, arrow=arrow())+
+#             annotate("text",x=-4.8,y=40,label="Reliability score decreased \nduring the pandemic")+
+#             annotate("segment", x = -0.5, xend = -10, y = 40, yend = 40, colour = "black", size=1, alpha=0.6, arrow=arrow()),
+#           nrow=2,rel_heights = c(2,1),align="hv",axis="l",labels="AUTO")
+# ggsave("outputs/Fig3_updated.png",height=9,width=7)
+
+
+
+ggplot(data = reliability_alluvial%>% filter(prior_reliability_1_10!="Did not answer",
+                                             during_reliability_1_10!="Did not answer"),
+       aes(axis1 = prior_reliability_1_10, axis2 = during_reliability_1_10,
+           y = freq, freq2 = freq2))+
+  scale_x_discrete(limits = c("Prior to the COVID-19 pandemic", "During the COVID-19 pandemic"), 
+                   expand = c(.2, .05),
+                   labels = function(x) str_wrap(x, width = 12)) +
+  labs(x="Time period",y="Number of respondents",fill="Reliability score during the pandemic")+
+  theme_classic()+
+  facet_wrap(~platform_label_generic,scales="free_y")+
+  geom_alluvium(aes(fill=during_reliability_1_10))+
+  geom_stratum()+
+  scale_fill_viridis_d(begin=1,end=0)+
+  geom_text(stat = "stratum", aes(label = after_stat(paste0(stratum))),
+            size=2.5,min.y=4) +
+  theme(legend.position = "bottom",
+        axis.line = element_blank(),strip.text = element_text(size=12),
+        axis.text.x = element_text(colour="black"))+
+  ggtitle("On a scale of 1-10 with 1 being “extremely unreliable” and 10 being “extremely \nreliable” how do you feel about the use of transmission models in informing \npublic health policy?")
+ggsave("outputs/SupFig5.png",height=6,width=7)
+
+
+
+reliability_alluvial_grouped <- all_responses %>% 
+  dplyr::select(platform_label_generic,prior_reliability_1_10,during_reliability_1_10) %>%
+    mutate(prior_reliability_1_10 = case_when(prior_reliability_1_10 %in% c(1,2) ~ "1-2",
+                                              prior_reliability_1_10 %in% c(3,4) ~ "3-4",
+                                              prior_reliability_1_10 %in% c(5,6) ~ "5-6",
+                                              prior_reliability_1_10 %in% c(7,8) ~ "7-8",
+                                              prior_reliability_1_10 %in% c(9,10) ~ "9-10",
+                                              is.na(prior_reliability_1_10) ~ "NA"),
+           during_reliability_1_10 = case_when(during_reliability_1_10 %in% c(1,2) ~ "1-2",
+                                              during_reliability_1_10 %in% c(3,4) ~ "3-4",
+                                              during_reliability_1_10 %in% c(5,6) ~ "5-6",
+                                              during_reliability_1_10 %in% c(7,8) ~ "7-8",
+                                              during_reliability_1_10 %in% c(9,10) ~ "9-10",
+                                              is.na(during_reliability_1_10) ~ "NA")) %>%
+    group_by(platform_label_generic,prior_reliability_1_10,during_reliability_1_10) %>%
+    summarise(freq = length(platform_label_generic)) %>% ungroup() %>%
+    group_by(platform_label_generic,prior_reliability_1_10) %>%
+    mutate(total = sum(freq),
+          freq2 = freq) %>% ungroup() %>%
+    group_by(platform_label_generic,during_reliability_1_10) %>%
+    mutate(during_total = sum(freq)) %>%
+  mutate(prior_reliability_1_10 = factor(ifelse(is.na(prior_reliability_1_10),"Did not answer",
+                                                prior_reliability_1_10),
+                                         levels=c("1-2","3-4","5-6","7-8","9-10","Did not answer")),
+         during_reliability_1_10 = factor(ifelse(is.na(during_reliability_1_10),"Did not answer",
+                                                 during_reliability_1_10),
+                                          levels=c("1-2","3-4","5-6","7-8","9-10","Did not answer"))) %>%
+  ungroup()
+
+
+
+ggplot(data = reliability_alluvial_grouped%>% filter(prior_reliability_1_10!="Did not answer",
+                                           during_reliability_1_10!="Did not answer"),
+       aes(axis1 = prior_reliability_1_10, axis2 = during_reliability_1_10,
+           y = freq, freq2 = freq2))+
+  scale_x_discrete(limits = c("Prior to the COVID-19 pandemic", "During the COVID-19 pandemic"),
+                   expand = c(.2, .05),
+                   labels = function(x) str_wrap(x, width = 12)) +
+  labs(x="Time period",y="Number of respondents",fill="Reliability score during the pandemic")+
+  theme_classic()+
+  facet_wrap(~platform_label_generic,scales="free_y")+
+  geom_alluvium(aes(fill=during_reliability_1_10))+
+  geom_stratum()+
+  scale_fill_viridis_d(begin=1,end=0)+
+  geom_text(stat = "stratum", aes(label = after_stat(paste0(stratum))),
+            min.y=7) +
+  theme(legend.position = "bottom",
+        axis.line = element_blank(),strip.text = element_text(size=12),
+        axis.text.x = element_text(colour="black"))+
+  ggtitle("Were you aware of the use of transmission models in informing public health policy?")
+
+plot_grid(ggplot(data = reliability_alluvial_grouped %>% filter(prior_reliability_1_10!="Did not answer",
                                                        during_reliability_1_10!="Did not answer"),
                  aes(axis1 = prior_reliability_1_10, axis2 = during_reliability_1_10,
                      y = freq, freq2 = freq2))+
-            scale_x_discrete(limits = c("Prior to the COVID-19 pandemic", "During the COVID-19 pandemic"), 
+            scale_x_discrete(limits = c("Prior to the COVID-19 pandemic", "During the COVID-19 pandemic"),
                              expand = c(.2, .05),
                              labels = function(x) str_wrap(x, width = 12)) +
             labs(x="Time period",y="Number of respondents",fill="Reliability score during the pandemic")+
@@ -238,7 +353,23 @@ plot_grid(ggplot(data = reliability_alluvial%>% filter(prior_reliability_1_10!="
           nrow=2,rel_heights = c(2,1),align="hv",axis="l",labels="AUTO")
 ggsave("outputs/Fig3_updated.png",height=9,width=7)
 
+
+
 ## Supplementary Table 8
+
+reliability_time_summary %>%
+  mutate(difference_label = ifelse(difference==0,"Same score",
+                                   ifelse(difference>0,"Increased reliability",
+                                          ifelse(difference<0,"Decreased reliability",NA)))) %>%
+  group_by(platform_label_generic,difference_label) %>%
+  summarise(count = sum(count)) %>%
+  mutate(platform_sample_size = ifelse(platform_label_generic=="Online panel",504,202),
+         perc = count/platform_sample_size *100)
+
+
+
+
+## Supplementary Table 9
 m1 <- lm(prior_reliability_1_10~age_group+gender,
          data = reliability_time %>% filter(platform_label=="Prolific Academic"))
 summary(m1)
@@ -259,7 +390,7 @@ m4 <- lm(during_reliability_1_10~age_group+gender,
 summary(m4)
 anova(m4,test="Chisq")
 
-## Supplementary Table 9 (UPDATED)
+## Supplementary Table 10 (UPDATED)
 
 reliability_time <- reliability_time %>%
   mutate(prior_awareness_use_in_policy_numeric = ifelse(prior_awareness_use_in_policy=="Yes",1,
@@ -291,7 +422,7 @@ anova(m8,test="Chisq")
 
 
 
-## Supplementary Figure 5
+## Supplementary Figure 6
 
 ggplot(reliability_time,aes(x=prior_reliability_1_10,y=during_reliability_1_10,col=platform_label_generic,
                             fill=platform_label_generic))+
@@ -309,10 +440,10 @@ ggplot(reliability_time,aes(x=prior_reliability_1_10,y=during_reliability_1_10,c
   scale_x_continuous(n.breaks = 10)+
   scale_y_continuous(n.breaks = 10)+
   geom_abline(slope=1,intercept = 0,linetype="dashed")
-ggsave("outputs/SupFig5.png",width=6,height=5)
+ggsave("outputs/SupFig6.png",width=6,height=5)
 
 
-## Supplementary Table 10
+## Supplementary Table 11
 m9 <- lm(during_reliability_1_10~prior_reliability_1_10,data=reliability_time %>%
            filter(platform_label=="Prolific Academic"))
 summary(m9)
@@ -322,16 +453,7 @@ m10 <- lm(during_reliability_1_10~prior_reliability_1_10,data=reliability_time %
 summary(m10)
 
 
-## Supplementary Table 11
 
-reliability_time_summary %>%
-  mutate(difference_label = ifelse(difference==0,"Same score",
-                                   ifelse(difference>0,"Increased reliability",
-                                          ifelse(difference<0,"Decreased reliability",NA)))) %>%
-  group_by(platform_label_generic,difference_label) %>%
-  summarise(count = sum(count)) %>%
-  mutate(platform_sample_size = ifelse(platform_label_generic=="Online panel",504,202),
-         perc = count/platform_sample_size *100)
 
 ## Supplementary Table 12
 m11 <- lm(difference~age_group+gender,data=reliability_time %>% filter(platform_label=="Prolific Academic"))
@@ -344,7 +466,7 @@ summary(m12)
 anova(m12,test="Chisq")
 
 
-## Supplementary Figure 6
+## Supplementary Figure 7
 reliability_time <- reliability_time %>%
   mutate(prior_awareness_use_in_policy_label = ifelse(prior_awareness_use_in_policy=="No",
                                                       "Self-reported no awareness",
@@ -391,7 +513,7 @@ ggplot(reliability_time_melt %>% filter(!is.na(awareness_use_in_policy_label)),
   scale_x_discrete(labels = function(x) str_wrap(x, width = 8))+
   theme(legend.position="bottom",strip.background = element_rect(fill="white"))+
   labs(x="Time period",y="On a scale of 1-10 with 1 being “extremely unreliable” \nand 10 being “extremely reliable” how do you feel \nabout the use of transmission models in informing \npublic health policy?",col="")
-ggsave("outputs/SupFig6.png",width=6,height=5)
+ggsave("outputs/SupFig7.png",width=6,height=5)
 
 
 ## tests for significance 
@@ -442,7 +564,7 @@ wilcox.test(reliability_time_melt %>% filter(platform_label=="Twitter",
             paired=FALSE,
             alternative="two.sided")
 
-## Supplementary Figure 7
+## Supplementary Figure 8
 reliability_during_level_awareness <- all_responses %>% dplyr::select(platform_label_generic,
                                                                       during_reliability_1_10,
                                                                       during_level_of_awareness) %>%
@@ -468,7 +590,7 @@ ggplot(reliability_during_level_awareness %>% filter(!is.na(during_level_of_awar
   scale_x_discrete(labels = function(x) str_wrap(x, width = 8))+
   theme(legend.position="bottom",strip.background = element_rect(fill="white"))+
   labs(x="Sample",y="During the pandemic, on a scale of 1-10 with 1 being \n“extremely unreliable” and 10 being “extremely reliable” \nhow do you feel about the use of transmission models \nin informing public health policy?",col="")
-ggsave("outputs/SupFig7.png",width=6,height=5)
+ggsave("outputs/SupFig8.png",width=6,height=5)
 
 
 ## tests for significance 
@@ -511,7 +633,7 @@ wilcox.test(reliability_during_level_awareness %>%
             alternative="two.sided")
 
 
-## Supplementary Figure 8
+## Supplementary Figure 9
 
 library(plyr)
 
@@ -589,7 +711,7 @@ ggplot(reliability_how_wide_melt %>% filter(!is.na(how)),
         legend.position = "bottom")+
   labs(x="How were you aware of transmission modelling? \nPlease select all that apply.",
        y="On a scale of 1-10 with 1 being “extremely \nunreliable” and 10 being “extremely reliable” \nhow do you feel about the use of transmission \nmodels in informing public health policy?",col="")
-ggsave("outputs/SupFig8.png",width=9,height=7)
+ggsave("outputs/SupFig9.png",width=9,height=7)
 
 
 detach("package:plyr", unload=TRUE)
